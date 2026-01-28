7bb572db3349f7cc0b73aee24f131e862a0ea257
Nguyenasdname <nguyenkuhaku@gmail.com>
2026-01-28 23:51:58 +0700

    fix deploy

--- src/api.ts
+++ src/api.ts
@@ -4,6 +4,7 @@ const api = axios.create({
     baseURL: 'https://fundtalk-api.onrender.com'
 })
 
+//oke
 api.interceptors.request.use((config) => {
     const token = localStorage.getItem('token')
     if (token) {
